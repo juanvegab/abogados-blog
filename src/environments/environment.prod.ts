b2ff98fcd46c7a86748bcb98410fc464d4f8e896
Juan Vega <jcvega@strongboxdata.com>
2017-10-31 17:53:49 -0600

    Environment Variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDxmr9JHAGpmIXe0BtP6YhOCXgYiOylyiw',
+    authDomain: 'blog-abogados.firebaseapp.com',
+    databaseURL: 'https://blog-abogados.firebaseio.com',
+    projectId: 'blog-abogados',
+    storageBucket: 'blog-abogados.appspot.com',
+    messagingSenderId: '414481414222'
+  }
 };
