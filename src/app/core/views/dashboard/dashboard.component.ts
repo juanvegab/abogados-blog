6fa1484fff1057c12abafe5edb8eee7921c3d142
Juan Vega <jcvega@strongboxdata.com>
2017-11-06 01:12:37 -0600

    [INTEGRATION] Sass integration to project, old css files updated to sass.

--- src/app/core/views/dashboard/dashboard.component.ts
+++ src/app/core/views/dashboard/dashboard.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from '../../../shared/';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
